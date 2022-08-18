24c0422b7ae91c38675f90ad8b47a368d3094ea2
Source Academy User <No public email provided>
2022-08-19 00:02:18 +0800

    Changes made from Source Academy

--- CS1101S Q1.js
+++ CS1101S Q1.js
@@ -1,4 +1,4 @@
-import {from_url, stack_frac, stack, beside_frac, beside, quarter_turn_right,quarter_turn_left, make_cross, rcross, stackn, show} from "rune";
+import {from_url, stack_frac, stack, beside_frac, heart, beside, quarter_turn_right,quarter_turn_left, make_cross, rcross, stackn, show} from "rune";
 
 const paw = from_url("https://i.imgur.com/GJg95B8.png");
 const x = 5;
@@ -13,6 +13,11 @@ function qtr_turn_add(rune,n)
     return beside_frac(1-1/n, quarter_turn_right(rune), stackn(n-1,paw));
 }
 */
+
+function besiden(n,rune)
+{
+    return quarter_turn_right(stackn(n,quarter_turn_left(rune)));
+}
 
 function turn_upside_down(rune)
 {
@@ -30,11 +35,56 @@ function persian(rune,x)
            ,stackn(x-1,quarter_turn_left(rune))))
            ,stackn(x,rune));
 }
+show(persian(paw,5));
+
+function addleftright(center,pattern,x)
+{
+    return beside_frac(1/(x),stackn(x-2,pattern),beside_frac(1- 1/(x-1), center,stackn(x-2,pattern)));
+}
+
+function addtopdown(center,pattern,x)
+{
+    return stack_frac(1/(x),besiden(x-2,pattern),stack_frac(1- 1/(x-1), center,besiden(x-2,pattern)));
+}
+
+show(addleftright(addtopdown(make_cross(paw),paw,5),paw,5));
+
+function addlrwtd(center,pattern,x)
+{
+    return beside_frac(1/(x),stackn(x,pattern),beside_frac(1- 1/(x-1), center,stackn(x,pattern)));
+}
+
+function both(center, pattern, x)
+{
+    return addlrwtd(addtopdown(center,pattern,x),pattern,x);
+}
+
+function refinedpersian(rune,x)
+{
+    return both(make_cross(rune),rune,x);
+}
+
+show(refinedpersian(,paw,5));
+
+/*
+function add4side(center,pattern,x)
+{
+    return addleftright(quarter_turn_right(addleftright(quarter_turn_right(center),pattern,x)),quarter_turn_right(pattern),x+2);
+}
+
+function addtopdown1(center,pattern,x)
+{
+    return quarter_turn_right(beside_frac(1/(x),stackn(x-2,quarter_turn_left(pattern)),beside_frac(1- 1/(x-1), center,stackn(x-2,quarter_turn_left(pattern)))));
+}
+
+function add4side1(center,pattern,x,counter)
+{
+    return counter===1
+    ? addleftright(center,quarter_turn_right(pattern),x)
+    : addleftright(quarter_turn_left(add4side1(center,quarter_turn_right(pattern),x,counter-1)),quarter_turn_right(pattern),x+2);
+}
+*/
+
 //show(beside_frac(1 - 1/n, make_cross(paw),stackn(n-2,paw)));
 
 //show(qtr_turn_add(start(paw,5),5));
-
-show(persian(paw,5));
-show(quarter_turn_right(persian(paw,5)));
-show(turn_upside_down(persian(paw,5)));
-show(quarter_turn_left(persian(paw,5)));
