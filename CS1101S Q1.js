9749d48381074d193a527951bf8af18952b8f42a
Source Academy User <No public email provided>
2022-08-18 17:19:09 +0800

    Changes made from Source Academy

--- CS1101S Q1.js
+++ CS1101S Q1.js
@@ -25,14 +25,14 @@ function turn_upside_down(rune)
 }
 function do_x_times(rune,x)
 {
-    return beside_frac(1-1/x, quarter_turn_right(
-           beside_frac(1-1/x, quarter_turn_right(
-           beside_frac(1-1/x, quarter_turn_right(
-           beside_frac(1-1/x, quarter_turn_right(
+    return beside_frac(1-1/5, quarter_turn_right(
+           beside_frac(1-1/5, quarter_turn_right(
+           beside_frac(1-1/4, quarter_turn_right(
+           beside_frac(1-1/4, quarter_turn_right(
            make_cross(rune))
-           ,stackn(x-2,rune)))
-           ,stackn(x-1,rune)))
-           ,stackn(x-1,rune)))
+           ,stackn(x-2,quarter_turn_right(rune))))
+           ,stackn(x-1,turn_upside_down(rune))))
+           ,stackn(x-1,quarter_turn_left(rune))))
            ,stackn(x,rune));
 }
 //show(beside_frac(1 - 1/n, make_cross(paw),stackn(n-2,paw)));
@@ -40,3 +40,6 @@ function do_x_times(rune,x)
 //show(qtr_turn_add(start(paw,5),5));
 
 show(do_x_times(paw,5));
+show(quarter_turn_right(do_x_times(paw,5)));
+show(turn_upside_down(do_x_times(paw,5)));
+show(quarter_turn_left(do_x_times(paw,5)));
