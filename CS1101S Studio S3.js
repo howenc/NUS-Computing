5d39d819cae86720c9189af5e573648ddaefa147
Source Academy User <No public email provided>
2022-08-22 14:05:10 +0800

    Changes made from Source Academy

--- CS1101S Studio S3.js
+++ CS1101S Studio S3.js
@@ -9,20 +9,26 @@ function moony_1(n)
               stack(blank,moony_1(n-1)));
 }
 
-function moony_3(n,rune)
+function moony_3(n)
 {
     return n===1
-          ? rune
-          : moony_3(n-1,beside_frac(1/n,stack_frac(1/n,circle,square),stack_frac(1/n,blank,rune)));
+          ? circle
+          : beside_frac(1/n,stack_frac(1/n,circle,square),stack_frac(1/n,blank,moony_3(n-1)));
 }
 
-
-
-function moony_31(n,counter,rune)
-{
+function moony_3help(n,counter,shape,rune) {
     return counter===n
-          ? rune
-          : moony_31(n,counter+1,beside_frac(1/counter,stack_frac(1/counter,circle,square),stack_frac(1/counter,blank,circle)));
+         ? rune
+         : moony_3help(n,counter+1,shape,
+                        beside_frac(1/counter,
+                                    stack_frac(1/counter,shape,square),
+                                    stack_frac(1/counter,blank,rune)));
 }
 
-show(moony_31(4,1,circle));
+function moony_3iter(n,rune){
+    return moony_3help(n,1,rune,rune);
+}
+show(moony_3iter(9,circle));
+
+
+//show(beside_frac(1/1,stack_frac(1/1,circle,square),stack_frac(1/1,blank,circle))));
