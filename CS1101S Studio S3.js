c458b530020225b660e246661c9c99fc448ca111
Source Academy User <No public email provided>
2022-08-22 08:30:03 +0800

    Changes made from Source Academy

--- CS1101S Studio S3.js
+++ CS1101S Studio S3.js
@@ -1,2 +1,8 @@
-import {overlay, overlay_frac, scale, scale_independent, circle, blank, corner, square, translate, anaglyph, hollusion, nova, triangle, stack_frac, stack, beside_frac, heart, beside, quarter_turn_right,quarter_turn_left, make_cross, rcross, stackn, show} from "rune";
+import {overlay, overlay_frac, scale, beside, scale_independent,ribbon, circle, blank, corner, square, translate, anaglyph, hollusion, nova, triangle, stack_frac, stack, beside_frac, heart, quarter_turn_right,quarter_turn_left, make_cross, rcross, stackn, show} from "rune";
 
+function moony_1(rune)
+{
+    return beside(stack(circle,square),stack(blank,rune));
+}
+
+show(moony_1(circle));
