6c1cfd4f6eca7ce7373787852de5014e951d0f50
Source Academy User <No public email provided>
2022-08-25 01:07:43 +0800

    Changes made from Source Academy

--- CS1101S Q3 hardest quest.js
+++ CS1101S Q3 hardest quest.js
@@ -2,30 +2,26 @@
 const twice = display => input => display(display(input));
 
 const increment_repeater = 
-        twice =>                                        //the input of i_r must be twice see line 29
-        display =>                                      //the input of twice must be display see line 30
-        inputofdisplay =>                               //the input of display must be the string "Alert" see line 31
+        twice =>                                        //the input of i_r must be twice see line 18
+        display =>                                      //the input of twice must be display see line 19
+        inputofdisplay =>                               //the input of display must be the string "Alert" see line 20
         twice(display)(display(inputofdisplay));        //since i have display, i will display 3 times as it is in memory
       //display(display(display(inputofdisplay)         //but this means that it is hardcoded to have 3
                                                         //so this means that i have to put twice into the code
                                                         //twice = display(display(input))
                                                         //so this means that 2 displays need to be removed
                                                         //twice(display)(display(inputofdisplay)) will work 
-                                                        //twice(display(inputofdisplay))(display) doesn't work as it contradicts line 28
+                                                        //twice(display(inputofdisplay))(display) doesn't work as it contradicts line 17
                                                         //display(twice(display)(inputofdisplay)) will work
                                                         //no other combinations as twice must have display as input
-                                                        
-        
+//twice take display as input and outputs the final thing
+//i_r takes twice as input and outputs twice with input display
+//thrice takes display as input and outputs function i_r with input twice
+//warn takes string "Alert" as input and outputs thrice with input display
+
+
 const thrice = increment_repeater(twice);
 const fourtimes = increment_repeater(thrice);
 const warn = thrice(display);
 warn("ALERT");          // should display "ALERT"
                         // three times
-                        
-
-
-
-//twice take display as input and outputs the final thing
-//i_r takes twice as input and outputs twice with input display
-//thrice takes display as input and outputs function i_r with input twice
-//warn takes string "Alert" as input and outputs thrice with input display
