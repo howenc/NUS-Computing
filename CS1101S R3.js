84b0347e992f32a772605048810e852b33911904
Source Academy User <No public email provided>
2022-08-24 19:24:44 +0800

    Changes made from Source Academy

--- CS1101S R3.js
+++ CS1101S R3.js
@@ -3,3 +3,29 @@ function my_sum(n) {
            ? 0
            : (n+1) * n + my_sum(n-1);
 }
+
+function sum(term, a, next, b) {
+    return a > b
+       ? 0
+       : term(a) + sum(term, next(a), next, b);
+}
+
+function my_sum1(n) {
+    return sum(a=>((a+1) * a),1, a=>a+1,n);
+}
+
+function sumiter(store, term, a, next, b) {
+    return a > b
+       ? store
+       : sumiter(term(a) + store, term, next(a), next, b);
+}
+
+function f(g) {
+    const x = 3;
+    return x=> x+g(x);
+}
+
+function g(f,y) {
+    const h = (y,f) => y(f);
+    return h(f,y);
+}
