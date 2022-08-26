2b0ae1b3208c87fff8949ea4150a446c860479fd
Source Academy User <No public email provided>
2022-08-26 10:17:57 +0800

    Changes made from Source Academy

---
+++ CS1101S Studio S4.js
@@ -0,0 +1,10 @@
+function pascal(row,position) {
+    return row < 1
+         ? 1
+         : position <= 1
+         ? 1
+         : position >= row + 1
+         ? 1
+         : pascal(row-1,position-1) + pascal(row-1,position);
+}
+pascal(4,1);
