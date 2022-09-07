5263cf2ef55a9c653b61c554d3b38a4c7ad596a3
Source Academy User <No public email provided>
2022-09-07 23:33:16 +0800

    Changes made from Source Academy

--- Reverse Append for lists.js
+++ Reverse Append for lists.js
@@ -2,8 +2,8 @@
 
 function append(xs, ys) {
 return is_null(xs)
-? ys
-: pair(head(xs), append(tail(xs), ys));
+    ? ys
+    : pair(head(xs), append(tail(xs), ys));
 }
 
 // Reverse the order of the lists using append
@@ -46,11 +46,14 @@ function traverse(xs) {
     }
 }
 
-function accumulate_for_tree(f,initial,xs) {
-    if (is_null(xs)) {
-        return initial;
-    } else {
-        is_list(head(xs)) ? accumulate_for_tree(f,initial,head(xs)) : f(head(xs),initial);
-        return accumulate_for_tree(f,initial,tail(xs));
-    }
+function flatten_list(xs) {
+    return is_null(xs)
+        ? null
+        : is_list(head(xs))
+            ? accumulate((x,y)=>append(x,y),null,xs)
+            : pair(head(xs),flatten_list(tail(xs)));
 }
+
+const my_tree = list(1,list(2,list(3,4),5),list(6,7));
+const LoL = list(list(1,2),list(3,4,5,6),null,list(7,8,9));
+flatten_list(my_tree);
