96d20f2f36351a395adb05dff6e81b95c391eb02
Source Academy User <No public email provided>
2022-09-12 00:48:16 +0800

    Changes made from Source Academy

--- CS1101S Q7.js
+++ CS1101S Q7.js
@@ -2,14 +2,6 @@ import {make_sound,play,adsr, noise_sound, cello, get_duration, bell,silence_sou
 
 // Q1
 
-function flatten_tree(xs) {
-        return is_null(xs)
-            ? null
-            : is_list(head(xs))
-                ? append(flatten_tree(head(xs)),flatten_tree(tail(xs)))
-                : append(list(head(xs)) , flatten_tree(tail(xs)));
-}
-
 function generate_list_of_note(letter, list_of_interval) {
     const number = is_number(letter)
                         ? letter
@@ -27,44 +19,15 @@ function repeat_pattern(n, generate, letter) {
     return n === 0 ? letter : repeat_pattern(n - 1, generate, generate(letter));
 }
 
-repeat_pattern(1,(x)=>append(pentatonic_list_of_interval,x),pentatonic_list_of_interval);
-
 function list_of_notes(n,letter,interval) {
     return generate_list_of_note(letter,repeat_pattern(n-1,(x)=>append(interval,x),interval));
 }
 
-list_of_notes(2,'C4',pentatonic_list_of_interval);
-
-/*
-function startofeachlistofnotes(n,list_of_interval,letter) {
-    return repeat_pattern(n-1,x=>head(reverse(generate_list_of_note(x,list_of_interval))),head(generate_list_of_note(letter,list_of_interval)));
-}
-
-function repeat_pattern(n, generate, letter) {
-    return n === 0 ? letter : repeat_pattern(n - 1, generate, generate(letter));
-}
-
-repeat_pattern(1,(x)=> head(reverse(generate_list_of_note(x,pentatonic_list_of_interval))),'C4');
-startofeachlistofnotes(1,pentatonic_list_of_interval,'C4');
-
-function entire_list_of_sounds(letter,list_of_interval,n,i) {
-    return i>n
-        ? null
-        : pair(generate_list_of_note(startofeachlistofnotes(i,list_of_interval,letter),list_of_interval),entire_list_of_sounds(startofeachlistofnotes(i,list_of_interval,letter),list_of_interval,n,i+1));
-}
-
-function flatten_and_play(list) {
-    return consecutively(flatten_tree(list));
-}
-
-entire_list_of_sounds('C4',pentatonic_list_of_interval,5,1);
-
-(flatten_tree(entire_list_of_sounds('C4',pentatonic_list_of_interval,2,1)));
-
 function repeated_scale(note, list_of_interval, n, duration, instrument) {
-    
+    return map(x=>instrument(x,duration),list_of_notes(n,note,list_of_interval));
 }
 
 play(consecutively(repeated_scale("C4", pentatonic_list_of_interval,
                                   2, 1, cello)));
-*/
+                                  
+play(consecutively(repeated_scale("C4", list(2,1,1,3), 3, 0.5, cello)));
