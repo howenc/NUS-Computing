76210677bbc80ff12a5dc7f5ab609beb2ffdf8fb
Source Academy User <No public email provided>
2022-09-15 19:44:10 +0800

    Changes made from Source Academy

--- CS1101S M10 && Sorting.js
+++ CS1101S M10 && Sorting.js
@@ -102,3 +102,26 @@ function selection_sort(xs) {
 }
 
 selection_sort(list(2,21,-100,100,10,9,6));
+
+// Insert sort
+
+function insert_cmp(x, xs, cmp) {
+    return is_null(xs)
+    ? list(x)
+    : cmp(x, head(xs))
+        ? pair(x, xs)
+        : pair(head(xs), insert_cmp(x, tail(xs), cmp));
+}
+
+function insertion_sort_cmp(xs, cmp) {
+    return is_null(xs)
+    ? xs
+    : insert_cmp(head(xs),
+                 insertion_sort_cmp(tail(xs), cmp),
+                 cmp);
+}
+const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
+insertion_sort_cmp(xs,(x,y)=>x<y);
+insertion_sort_cmp(xs,(x,y)=>y<x);
+insertion_sort_cmp(xs,(x,y)=>false);
+// Merge sort
