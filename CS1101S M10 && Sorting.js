965de6f9cf3f2dfc99d5cd642b0da93ae8055f1c
Source Academy User <No public email provided>
2022-09-15 19:39:00 +0800

    Changes made from Source Academy

--- CS1101S M10 && Sorting.js
+++ CS1101S M10 && Sorting.js
@@ -38,3 +38,67 @@ function quicksort(xs) {
 const my_list = list(23, 12, 56, 92, -2, 0,-10,29,4820,-3089);
 quicksort(my_list);
 
+// selection_sort
+
+// put the first n elements of xs into a list
+function take(xs, n) {
+    return is_null(xs)
+    ? null
+    : n<=0
+        ? null
+        : pair(head(xs),take(tail(xs),n-1));
+}
+
+take(list(1,2,3,4,5,6,7,8,9,10),1);
+// drop the first n elements from list, return rest
+
+function drop(xs, n) {
+    return is_null(xs)
+    ? null
+    : n<=0
+    ? xs
+    : drop(tail(xs),n-1);
+}
+
+drop(list(1,2,3,4,5,6,7,8,9,10),5);
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+
+// given a non-empty list xs, returns the smallest item in xs
+function smallest(xs) {
+    function smallest_helper(smallest,xs) {
+        const x = smallest < head(xs) ? smallest : head(xs);
+        return is_null(tail(xs))
+            ? x
+            : smallest_helper(x,tail(xs));
+    }
+    return smallest_helper(head(xs),xs);
+}
+
+smallest(list(1,2,3,4,5,6,7,8,9,10,0));
+
+function remove(x, xs) {
+    return is_null(xs)
+    ? null
+    : is_null(x)
+    ? pair(head(xs),remove(x,tail(xs)))
+    : head(xs) === x
+    ? remove(null,tail(xs))
+    : pair(head(xs),remove(x,tail(xs)));
+}
+
+remove(2,list(1,2,2,2,3,4,5,7,8,9));
+
+function selection_sort(xs) {
+    if (is_null(xs)) {
+        return null;
+    } else {
+        return is_null(tail(xs))
+        ? pair(head(xs),null)
+        : pair(smallest(xs),selection_sort((remove(smallest(xs),xs))));
+    }
+}
+
+selection_sort(list(2,21,-100,100,10,9,6));
