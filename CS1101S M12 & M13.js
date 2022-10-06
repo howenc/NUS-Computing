a13bc44fe72818700d59a030de765ee943d58087
Source Academy User <No public email provided>
2022-10-07 02:23:04 +0800

    Changes made from Source Academy

--- CS1101S M12 & M13.js
+++ CS1101S M12 & M13.js
@@ -163,7 +163,68 @@ function move_forward_by_10_and_check_again_left() {
 move_and_get_distance1();
 */
 // Mission 3
+//Q1
 const colorsensor = ev3_colorSensor();
-display(ev3_colorSensorBlue(colorsensor));
-display(ev3_colorSensorRed(colorsensor));
-display(ev3_colorSensorGreen(colorsensor));
+const touchsensor = ev3_touchSensor1();
+
+function get_lightintensity() {
+    if (ev3_touchSensorPressed(touchsensor)) {
+        return 0;
+    } else {
+        display(ev3_ambientLightIntensity(colorsensor));
+        pause(1000);
+        get_lightintensity();
+    }
+}
+
+//get_lightintensity();
+//Q2
+
+function get_lightintensity1() {
+    if (ev3_touchSensorPressed(touchsensor)) {
+        return 0;
+    } else if (display(ev3_ambientLightIntensity(colorsensor)) <= 1) {
+        runright(0.15);
+        runleft(0.1);
+        pause(pause_duration*0.05);
+        get_lightintensity1();
+    } else {
+        turn_right_until_black_line_or_90degree();
+    }
+}
+
+let right_turn_radius = 0;
+function turn_left_all_the_way() {
+    if (ev3_touchSensorPressed(touchsensor)) {
+        return 0;
+    } else if ((display(ev3_ambientLightIntensity(colorsensor))) <= 1) {
+        ev3_motorStop(leftwheel);
+        ev3_motorStop(rightwheel);
+        get_lightintensity1();
+    } else {
+        rotateleft(-0.05);
+        pause(pause_duration*0.025);
+        turn_left_all_the_way();
+    }    
+}
+function turn_right_until_black_line_or_90degree() {
+    if (ev3_touchSensorPressed(touchsensor)) {
+        return 0;
+    } else if (right_turn_radius >= 45) {
+        right_turn_radius = 0;
+        turn_left_all_the_way();
+    } else if ((display(ev3_ambientLightIntensity(colorsensor))) <= 1) {
+        right_turn_radius = 0;
+        ev3_motorStop(leftwheel);
+        ev3_motorStop(rightwheel);
+        get_lightintensity1();
+    } else {
+        rotateleft(0.05);
+        pause(pause_duration*0.025);
+        right_turn_radius = right_turn_radius+1;
+        turn_right_until_black_line_or_90degree();
+    }
+}
+
+//turn_right_until_black_line_or_270degree();
+get_lightintensity1();
