407d93c93753ef0d5dfffbfb5a58f1234c4be9de
Source Academy User <No public email provided>
2022-11-01 17:48:51 +0800

    Changes made from Source Academy

--- Prep for PA.js
+++ Prep for PA.js
@@ -96,3 +96,155 @@ function all_genes(list_of_strand) {
     return pair(head(temp),all_genes(tail(temp)));
 
 }
+
+function all_different(nums) {
+    if (is_null(nums)) {
+        return true;
+    } else if (is_null(filter(x=> x===head(nums),tail(nums)))) {
+        return all_different(tail(nums));
+    } else {
+        return false;
+    }
+}
+
+
+function is_valid_toto_set(nums, n, min, max) {
+    if (n !== length(nums)) {
+        return false;
+    } else if (!is_null(filter(x=>x<min,nums)) || !is_null(filter(x=>x>max,nums))) {
+        return false;
+    } else {
+        return all_different(nums);
+    }
+}
+
+function num_of_matches(numsA, numsB) {
+    function num_of_matches_1(numsA, numsB, counter) {
+        if (is_null(numsA)) {
+            return counter;
+        } else if (is_null(filter(x=> x === head(numsA),numsB))) {
+            return num_of_matches_1(tail(numsA),numsB, counter);
+        } else {
+            return num_of_matches_1(tail(numsA), numsB, counter + 1);
+        }
+    }
+    return num_of_matches_1(numsA, numsB, 0);    
+}
+
+function check_winning_group(bet_nums, draw_nums, extra_num) {
+    const n = length(draw_nums);
+    const base = num_of_matches(bet_nums,draw_nums);
+    const baseextrea = num_of_matches(bet_nums,list(extra_num));
+    if (base === n) {
+        return 1;
+    } else if (base === n-1) {
+        if (baseextrea === 1) {
+            return 2;
+        } else {
+            return 3;
+        }
+    } else if (base === n-2) {
+        if (baseextrea === 1) {
+            return 4;
+        } else {
+            return 5;
+        }
+    } else {
+        return 0;
+    }
+}
+
+function evaluate_BAE_tree(bae_tree) {
+    function evaluate(left,opp,right) {
+        return opp === '+'
+                ? left + right
+                : opp === '-'
+                    ? left - right
+                    : opp === '*'
+                        ? left * right
+                        : opp === '/' 
+                            ? left/right
+                            : 'error';
+    }
+    if (is_list(bae_tree)) {
+        const left = head(bae_tree);
+        const opp = head(tail(bae_tree));
+        const right = head(tail(tail(bae_tree)));
+        const left_eval = (is_number(left))
+                            ? left
+                            : evaluate_BAE_tree(left);
+        const right_eval = is_number(right)
+                            ? right
+                            : evaluate_BAE_tree(right);
+        return evaluate(left_eval,opp,right_eval);
+    } else {
+        return bae_tree;
+    }
+}
+
+
+function build_BAE_tree(bae_list) {
+    function earliestclose(bae_list) {
+        for (let i = 0; i < length(bae_list); i = i + 1) {
+            if (list_ref(bae_list,i) === ')') {
+                return i;
+            }
+        }
+    }
+    function closestopen(bae_list,i) {
+        for (let j = i-1; j >=0; j = j - 1) {
+            if (list_ref(bae_list,j) === '(') {
+                return j;
+            }
+        }
+    }
+    if (length(bae_list) <= 1) {
+        return head(bae_list);
+    }
+    const end = earliestclose(bae_list);
+    const start = closestopen(bae_list,end);
+    let temp = null;
+    for (let k = start+1; k < end; k = k + 1) {
+        temp = pair(list_ref(bae_list,k),temp);
+    }
+    temp = reverse(temp);
+    let new_lst = null;
+    for (let m = 0; m < length(bae_list); m = m + 1) {
+        if (m === end) {
+            new_lst = pair(temp, new_lst);
+        } else if (m<end && m >= start) {
+            new_lst = new_lst;
+        } else {
+            new_lst = pair(list_ref(bae_list,m),new_lst);
+        }
+    }
+    new_lst = reverse(new_lst);
+    return build_BAE_tree(new_lst);
+}
+
+function evaluate_BAE(bae_list) {
+    return evaluate_BAE_tree(build_BAE_tree(bae_list));
+}
+
+function check_parentheses(paren_list) {
+    let counter = 0;
+    function helper(paren_list) {
+        if (counter < 0) {
+            return false;
+        } else if (is_null(paren_list)) {
+            return counter > 0 ? false : true; 
+        } else if (head(paren_list) === '(') {
+            counter = counter + 1;
+            return helper(tail(paren_list));
+        } else if (head(paren_list) === ')') {
+            counter =  counter - 1;
+            return helper(tail(paren_list));
+        } else {
+            return helper(tail(paren_list));
+        }
+    }
+    return helper(paren_list);
+}
+const paren_list = list("(", "(", ")", ")");
+check_parentheses(paren_list);
+// returns true
